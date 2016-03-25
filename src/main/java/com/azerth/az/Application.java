85d482a669c69a4c3b4d8a98892a688163e4dbfc
kever <416314413@163.com>
2016-03-25 16:39:54 +0800

    1

---
+++ src/main/java/com/azerth/az/Application.java
@@ -0,0 +1,15 @@
+package com.azerth.az;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hxt on 2016/3/25.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/azerth/az/Greeting.java
@@ -0,0 +1,22 @@
+package com.azerth.az;
+
+/**
+ * Created by hxt on 2016/3/25.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
